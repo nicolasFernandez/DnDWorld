bac64240bfe71b5f656a65d52b1a9fc616aa5b5f
Nicolás Fernández <nicolas.fa@globant.com>
2022-12-26 14:00:29 -0300

    Adding races, abilities, stats, and subraces

---
+++ DnDWorld/DnDWorld/Clases/Abilities.swift
@@ -0,0 +1,17 @@
+//
+//  Abilities.swift
+//  DnDWorld
+//
+//  Created by Nicolas Alonso Fernandez Alarcon on 26-12-22.
+//
+
+import Foundation
+
+struct Abilities {
+    let strength: Stat
+    let dexterity: Stat
+    let constitution: Stat
+    let intelligence: Stat
+    let wisdom: Stat
+    let charisma: Stat
+}
---
+++ DnDWorld/DnDWorld/Clases/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  DnDWorld
+//
+//  Created by Nicolas Alonso Fernandez Alarcon on 26-12-22.
+//
+
+import Foundation
+
+struct Character {
+    let name: String
+    let abilities: Abilities
+}
