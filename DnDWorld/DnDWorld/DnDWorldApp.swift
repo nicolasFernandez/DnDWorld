3da4bebb9860563514751a790762305646beb72e
Nicolás Fernández <nicolas.fa@globant.com>
2022-12-26 12:15:22 -0300

    Initial commit

---
+++ DnDWorld/DnDWorld/DnDWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDWorldApp.swift
+//  DnDWorld
+//
+//  Created by Nicolas Alonso Fernandez Alarcon on 26-12-22.
+//
+
+import SwiftUI
+
+@main
+struct DnDWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
