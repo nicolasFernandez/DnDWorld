4f9fb874ba5eb8c05d02d09bdc245ac1d0766f58
Nicolás Fernández <nicolas.fa@globant.com>
2023-01-20 17:51:34 -0300

    Classtype enum is now string enum to use rawValue

--- DnDWorld/DnDWorld/Clases/Character.swift
+++ DnDWorld/DnDWorld/Clases/Character.swift
@@ -9,10 +9,11 @@ import Foundation
 
 struct Character {
     let name: String
+    let classType: [ClassType]
+
     let abilities: Abilities
     let skills: Skills
     let race: Race
     var armorClass: Int
     var equipmentList: [Equipment]
-    let classType: [ClassType]
 }
--- DnDWorld/DnDWorld/Clases/ClassType.swift
+++ DnDWorld/DnDWorld/Clases/ClassType.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ClassType {
+enum ClassType: String {
     case barbarian
     case bard
     case cleric
@@ -21,7 +21,8 @@ enum ClassType {
     case warlock
     case wizard
 
-    var description: String {
+    // TODO: move text to localization strings
+    var title: String {
         switch self {
         case .barbarian:
             return "Bárbaro"
@@ -40,13 +41,132 @@ enum ClassType {
         case .ranger:
             return "Explorador"
         case .rogue:
-            return ""
+            return "Pícaro"
         case .sorcerer:
             return "Hechicero"
         case .warlock:
-            return "Pícaro"
+            return "Brujo"
         case .wizard:
             return "Mago"
+        }
+    }
+
+    // TODO: move text to localization strings
+    var description: String {
+        switch self {
+        case .barbarian:
+            return "Un guerrero feroz de fondo primitivo que puede entrar en una furia de la batalla"
+        case .bard:
+            return "Un inspirante mago cuyo poder replica la música de la creación"
+        case .cleric:
+            return "Un campeón sacerdotal que esgrime magia divina al servicio de un poder mayor"
+        case .druid:
+            return "Un sacerdote de la Antigua Fe, que blande los poderes de la naturaleza -la luz de la luna y el crecimiento de las plantas, el fuego y el rayo- y que adopta formas animales."
+        case .fighter:
+            return "Un maestro del combate marcial competente con una gran variedad de armas y armaduras."
+        case .monk:
+            return "Un maestro de las artes marciales, que domina el poder del cuerpo en busca de la perfección física y espiritual."
+        case .paladin:
+            return "Un guerrero santo atado a un juramento sagrado"
+        case .ranger:
+            return "Un guerrero que usa la proeza marcial y la magia de la naturaleza para combatir las amenazas en los límites de la civilización"
+        case .rogue:
+            return "Un rufián que usa sigilo y astucia para superar obstáculos y enemigos."
+        case .sorcerer:
+            return "Un lanzador de conjuros que recurre a la magia inherente de un don o una linea de sangre."
+        case .warlock:
+            return "Un practicante de la magia que deriva de un contrato con una entidad extraplanaria"
+        case .wizard:
+            return "Un usuario de magia educado capaz de manipular la estructura de la realidad."
+        }
+    }
+
+    var hitDie: String {
+        switch self {
+        case .barbarian:
+            return "d12"
+        case .bard:
+            return "d8"
+        case .cleric:
+            return "d8"
+        case .druid:
+            return "d8"
+        case .fighter:
+            return "d10"
+        case .monk:
+            return "d8"
+        case .paladin:
+            return "d10"
+        case .ranger:
+            return "d10"
+        case .rogue:
+            return "d8"
+        case .sorcerer:
+            return "d6"
+        case .warlock:
+            return "d8"
+        case .wizard:
+            return "d6"
+        }
+    }
+
+    // TODO: move text to localization strings
+    var primaryAbility: String {
+        switch self {
+        case .barbarian:
+            return "Fuerza"
+        case .bard:
+            return "Carisma"
+        case .cleric:
+            return "Sabiduría"
+        case .druid:
+            return "Sabiduría"
+        case .fighter:
+            return "Fuerza o Destreza"
+        case .monk:
+            return "Destreza y Sabiduría"
+        case .paladin:
+            return "Fuerza y Carisma"
+        case .ranger:
+            return "Destreza y Sabiduría"
+        case .rogue:
+            return "Destreza"
+        case .sorcerer:
+            return "Hechicero"
+        case .warlock:
+            return "Carisma"
+        case .wizard:
+            return "Inteligencia"
+        }
+    }
+
+    // TODO: move text to localization strings
+    var saves: String {
+        switch self {
+        case .barbarian:
+            return "Fuerza y Constitución"
+        case .bard:
+            return "Destreza y Carisma"
+        case .cleric:
+            return "Sabiduría y Carisma"
+        case .druid:
+            return "Inteligencia y Sabiduría"
+        case .fighter:
+            return "Fuerza y Constitución"
+        case .monk:
+            return "Fuerza y Destreza"
+        case .paladin:
+            return "Sabiduría y Carisma"
+        case .ranger:
+            return "Fuerza y Destreza"
+        case .rogue:
+            return "Destreza e Inteligencia"
+        case .sorcerer:
+            return "Constitución y Carisma"
+        case .warlock:
+            return "Sabiduría y Carisma"
+        case .wizard:
+            return "Inteligencia y Sabiduría"
         }
     }
 }
