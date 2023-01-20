3665a90448a058a9cf278b0749cd97376332da9c
Nicolás Fernández <nicolas.fa@globant.com>
2023-01-20 17:56:40 -0300

    Created attack type for spells

---
+++ DnDWorld/DnDWorld/Clases/AttackType.swift
@@ -0,0 +1,13 @@
+//
+//  AttackType.swift
+//  DnDWorld
+//
+//  Created by Nicolas Alonso Fernandez Alarcon on 13-01-23.
+//
+
+import Foundation
+
+enum AttackType {
+    case melee
+    case ranged
+}
