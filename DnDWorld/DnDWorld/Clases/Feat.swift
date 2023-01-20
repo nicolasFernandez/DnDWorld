1e9eedbb5a204cce40b8eb2223ddcf13c8cbe7de
Nicolás Fernández <nicolas.fa@globant.com>
2023-01-20 17:53:09 -0300

    Added feats

---
+++ DnDWorld/DnDWorld/Clases/Feat.swift
@@ -0,0 +1,53 @@
+//
+//  Feat.swift
+//  DnDWorld
+//
+//  Created by Nicolas Alonso Fernandez Alarcon on 13-01-23.
+//
+
+import Foundation
+
+// Dotes
+enum Feat: String {
+    case actor
+    case alert
+    case athlete
+    case charger
+    case crossbowExpert
+    case defensiveDuelist
+    case dualWielder
+    case dungeonDelver
+    case durable
+    case elementalAdept
+    case greatWeaponMaster
+    case healer
+    case heaviliyArmored
+    case heavyArmorMaster
+    case inspiringLeader
+    case keenMind
+    case lightlyArmored
+    case linguist
+    case lucky
+    case mageSlayer
+    case magicInitiate
+    case martialAdept
+    case mediumArmorMaster
+    case mobile
+    case moderatelyArmored
+    case mountedCombatant
+    case observant
+    case polearmMaster
+    case resilient
+    case ritualCaster
+    case savageAttacker
+    case sentinel
+    case sharpshooter
+    case shieldMaster
+    case skilled
+    case skulker
+    case spellSniper
+    case tavernBrawler
+    case tough
+    case warCaster
+    case weaponMaster
+}
