812d9741835f9cf65455c04665c9ecc58eb6dafc
Nicolás Fernández <nicolas.fa@globant.com>
2023-01-20 17:53:55 -0300

    Added enum for spells component

---
+++ DnDWorld/DnDWorld/Clases/Component.swift
@@ -0,0 +1,14 @@
+//
+//  Component.swift
+//  DnDWorld
+//
+//  Created by Nicolas Alonso Fernandez Alarcon on 13-01-23.
+//
+
+import Foundation
+
+enum Component {
+    case V 
+    case S
+    case M
+}
