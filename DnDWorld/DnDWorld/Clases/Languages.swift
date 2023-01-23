bc9d38dc85cc2be6eb4c012c68bd2bdbabc75c9d
Nicolás Fernández <nicolas.fa@globant.com>
2023-01-23 17:46:16 -0300

    Added languages

--- DnDWorld/DnDWorld/Clases/Languages.swift
+++ DnDWorld/DnDWorld/Clases/Languages.swift
@@ -24,4 +24,41 @@ enum LanguageType {
     case primordial
     case sylvan
     case undercommon
+
+    var name: String {
+        switch self {
+        case .common:
+            return NSLocalizedString("common", comment: "")
+        case .dwarvish:
+            return NSLocalizedString("dwarvish", comment: "")
+        case .elvish:
+            return NSLocalizedString("elvish", comment: "")
+        case .giant:
+            return NSLocalizedString("giant", comment: "")
+        case .gnomish:
+            return NSLocalizedString("gnomish", comment: "")
+        case .goblin:
+            return NSLocalizedString("goblin", comment: "")
+        case .halfling:
+            return NSLocalizedString("halfling", comment: "")
+        case .orc:
+            return NSLocalizedString("orc", comment: "")
+        case .abyssal:
+            return NSLocalizedString("abyssal", comment: "")
+        case .celestial:
+            return NSLocalizedString("celestial", comment: "")
+        case .draconic:
+            return NSLocalizedString("draconic", comment: "")
+        case .deepSpeech:
+            return NSLocalizedString("deep_speech", comment: "")
+        case .infernal:
+            return NSLocalizedString("infernal", comment: "")
+        case .primordial:
+            return NSLocalizedString("primordial", comment: "")
+        case .sylvan:
+            return NSLocalizedString("sylvan", comment: "")
+        case .undercommon:
+            return NSLocalizedString("undercommon", comment: "")
+        }
+    }
 }
