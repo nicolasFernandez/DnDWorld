c7303c6b55805a594c257119efe155577e1a8951
Nicolas Alonso <Nicolas.Alonso.Fernandez.-ND@disney.com>
2025-10-02 19:49:33 -0300

    Add ability test for modifier

--- DnDWorldTests/Clases/AbilityTests.swift
+++ DnDWorldTests/Clases/AbilityTests.swift
@@ -15,4 +15,47 @@ final class AbilityTests: XCTestCase {
         XCTAssertNotNil(sut)
     }
 
+    func test_canInitWithNoRaceBonus() {
+        let sut = Ability(base: 10)
+        XCTAssertNotNil(sut)
+    }
+
+    func test_sumBasePlusRaceBonus() {
+        let sut = Ability(base: 8, raceBonus: 2)
+        XCTAssertEqual(sut.totalScore, 10)
+    }
+
+    func test_modifier_scoreOne_shouldBeMinusFive() {
+        let sut = Ability(base: 1)
+        XCTAssertEqual(sut.modifier, -5)
+    }
+
+    func test_modifier_scoreTwoOrThree_shouldBeMinusFour() {
+        let sutOne = Ability(base: 2)
+        XCTAssertEqual(sutOne.modifier, -4)
+
+        let sutTwo = Ability(base: 3)
+        XCTAssertEqual(sutTwo.modifier, -4)
+    }
+
+    func test_modifier_scoreTenOrEleven_shouldBeZero() {
+        let sutOne = Ability(base: 10)
+        XCTAssertEqual(sutOne.modifier, 0)
+
+        let sutTwo = Ability(base: 11)
+        XCTAssertEqual(sutTwo.modifier, 0)
+    }
+
+    func test_modifier_scoreTwentyEightOrTwentyNine_shouldBePlusNine() {
+        let sutOne = Ability(base: 28)
+        XCTAssertEqual(sutOne.modifier, 9)
+
+        let sutTwo = Ability(base: 29)
+        XCTAssertEqual(sutTwo.modifier, 9)
+    }
+
+    func test_modifier_scoreThirty_shouldBePlusTen() {
+        let sut = Ability(base: 30)
+        XCTAssertEqual(sut.modifier, 10)
+    }
 }
