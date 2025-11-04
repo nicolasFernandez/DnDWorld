e221de1439657e5ff4000a207a570e5e2594b4b5
Nicolas Fernandez <nicolas.fa@globant.com>
2025-11-04 11:38:28 -0300

    Modify Class Details
    - Embed inside a scroll view with padding for better view
    - Localize class features text

--- DnDWorld/Presentation/Views/Class/ClassDetails.swift
+++ DnDWorld/Presentation/Views/Class/ClassDetails.swift
@@ -10,58 +10,66 @@ import SwiftUI
 struct ClassDetails: View {
     var classType: ClassType
     var body: some View {
-        VStack (alignment: .leading, spacing: 8) {
-            ClassRow(classType: classType)
-            Text(classType.description)
-            Text("Class Features")
+        ScrollView {
+            VStack (alignment: .leading, spacing: 16) {
+                ClassRow(classType: classType)
+                Text(classType.description)
+                Text(
+                    NSLocalizedString(
+                        "Class Features",
+                        comment: ""
+                    )
+                )
                 .font(.title)
                 .fontWeight(.bold)
-            DetailRow(
-                title: NSLocalizedString("Primary Ability", comment: ""),
-                value: classType.primaryAbility)
-            Text("Hit Points")
-                .font(.title2)
-                .fontWeight(.bold)
-            DetailRow(
-                title: NSLocalizedString("Hit Dice", comment: ""),
-                value: "1d\(classType.hitDie) per \(classType.name) level"
-            )
-            DetailRow(
-                title: NSLocalizedString("Hit Points at 1st Level", comment: ""),
-                value: "1d\(classType.hitDie)"
-            )
-            DetailRow(
-                title: NSLocalizedString("Hit Points at Higher Levels", comment: ""),
-                value: "1d\(classType.hitDie)"
-            )
-            Text("Proficiencies")
-                .font(.title2)
-                .fontWeight(.bold)
-            DetailRow(
-                title: NSLocalizedString("Armor", comment: ""),
-                value: classType.primaryAbility)
-            DetailRow(
-                title: NSLocalizedString("Weapons", comment: ""),
-                value: classType.primaryAbility)
-            DetailRow(
-                title: NSLocalizedString("Tools", comment: ""),
-                value: classType.primaryAbility)
-            DetailRow(
-                title: NSLocalizedString("Saving Throws", comment: ""),
-                value: classType.saves
-            )
-            DetailRow(
-                title: NSLocalizedString("Skills", comment: ""),
-                value: classType.saves
-            )
-            Text("Equipment")
-                .font(.title2)
-                .fontWeight(.bold)
-            Text("You start with the following equipment, in addition to the equipment granted by your background:")
-            Text("·(a) a .... or (b) ...")
-            Text("·(a) a .... or (b) ...")
-            Text("·(a) a .... or (b) ...")
-            Text("·Something else")
+                DetailRow(
+                    title: NSLocalizedString("Primary Ability", comment: ""),
+                    value: classType.primaryAbility)
+                Text("Hit Points")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                DetailRow(
+                    title: NSLocalizedString("Hit Dice", comment: ""),
+                    value: "1d\(classType.hitDie) per \(classType.name) level"
+                )
+                DetailRow(
+                    title: NSLocalizedString("Hit Points at 1st Level", comment: ""),
+                    value: "1d\(classType.hitDie)"
+                )
+                DetailRow(
+                    title: NSLocalizedString("Hit Points at Higher Levels", comment: ""),
+                    value: "1d\(classType.hitDie)"
+                )
+                Text("Proficiencies")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                DetailRow(
+                    title: NSLocalizedString("Armor", comment: ""),
+                    value: classType.primaryAbility)
+                DetailRow(
+                    title: NSLocalizedString("Weapons", comment: ""),
+                    value: classType.primaryAbility)
+                DetailRow(
+                    title: NSLocalizedString("Tools", comment: ""),
+                    value: classType.primaryAbility)
+                DetailRow(
+                    title: NSLocalizedString("Saving Throws", comment: ""),
+                    value: classType.saves
+                )
+                DetailRow(
+                    title: NSLocalizedString("Skills", comment: ""),
+                    value: classType.saves
+                )
+                Text("Equipment")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text("You start with the following equipment, in addition to the equipment granted by your background:")
+                Text("·(a) a .... or (b) ...")
+                Text("·(a) a .... or (b) ...")
+                Text("·(a) a .... or (b) ...")
+                Text("·Something else")
+            }
+            .padding()
         }
     }
 }
